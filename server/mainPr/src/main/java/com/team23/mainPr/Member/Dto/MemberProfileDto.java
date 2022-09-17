4d8c4bc992491534c167a10b676acc11422e67bb
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-09-18 03:02:13 +0900

    [patch] member + profile 작업

--- server/mainPr/src/main/java/com/team23/mainPr/Member/Dto/MemberProfileDto.java
+++ server/mainPr/src/main/java/com/team23/mainPr/Member/Dto/MemberProfileDto.java
@@ -1,9 +1,11 @@
 package com.team23.mainPr.Member.Dto;
 
 import com.team23.mainPr.Dto.ParentCommonDto;
+import lombok.Data;
 
 import java.time.ZonedDateTime;
 
+@Data
 public class MemberProfileDto extends ParentCommonDto {
 
     private String loginId;
